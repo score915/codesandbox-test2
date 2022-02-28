cbbb6c7d1767d940fc5f9b57a1f9396f61887bf7
score915 <35161608+score915@users.noreply.github.com>
2022-02-28 09:23:43 +0900

    as of 2022/02/27

--- src/index.js
+++ src/index.js
@@ -1,11 +1,102 @@
-import "./styles.css";
+/**
+ *
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<p>test</p>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var
+
+// val1 = "overwrite test";
+// console.log(val1);
+
+// // re-predict
+
+// var val1 = "var_second";
+// console.log(val1);
+
+// let val2 = "let parameter";
+// console.log(val2);
+// val2 = "let para overwritten"
+// console.log(val2);
+// let val3 = "test";
+// const val4 = "const3";
+// const val5 = 10;
+// console.log(val5);
+
+// const val4 = {
+//   name: "James",
+//   age: 29
+// };
+
+// console.log(val4);
+
+// val4.name = "John";
+// val4.address = "Fujisawa";
+
+// const val5 = ["dog", "cat"];
+// console.log(val5);
+// val5[0] = "bird";
+// val5.push("monkey");
+
+// const message2 = `My name is ${val4.name}, age is ${val4.age}.`;
+// console.log(message2);
+
+function func1(str) {
+  return str;
+}
+
+const func2 = function func２(str) {
+  return str;
+};
+
+const func3 = (str) => {
+  return `str = ${str}`;
+};
+
+const func4 = (num1, num2) => num1 + num2;
+
+console.log(func1("test"));
+
+console.log(func4(3, 3));
+
+//分割代入
+const myProfile = ["James", 28];
+const message3 = `Name is ${myProfile[0]}.`;
+console.log(message3);
+
+const [name, age] = myProfile;
+const message4 = `Name is ${name}, too.`;
+console.log(message4);
+
+///     スプレッド構文：配列の展開
+const arr1 = [1, 2, 3];
+console.log(arr1);
+const someFunc = (num1, num2) => num1 + num2;
+console.log(...arr1);
+console.log(...arr1);
+console.log(someFunc(...arr1));
+
+const arr2 = [1, 2, 30, 4, 5];
+console.log(arr1);
+const [num1, num2, ...arr3] = arr2;
+console.log(arr3);
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+const arr6 = [...arr4];
+// const arr7 = arr4;
+// console.log(arr6);
+// console.log(arr7);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+for (let index = 0; index < arr2.length; index++) {
+  const element = arr2[index];
+  console.log(element);
+}
+
+const nameArr2 = ["Tanaka", "yamada", "Katoh"].map((name) => name);
+
+console.log(nameArr2);
+
+const nameArr3 = ["Tanaka", "yamada", "Katoh"].map((name) => console.log(name));
